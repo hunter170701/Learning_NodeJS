888dab375aa95e5713e291b57124451a942c57de
minh duc <hunter170701@gmail.com>
2021-05-31 20:24:29 +0700

    upload

---
+++ src/app/controllers/SiteController.js
@@ -0,0 +1,14 @@
+class SiteController {
+  
+  // TODO: [GET] /
+  index(req, res) {
+    res.render("home");
+  }
+
+  // TODO: [GET] /search
+  search(req, res) {
+    res.render("search");
+  }
+}
+
+module.exports = new SiteController();
